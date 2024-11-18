bd78cdffd780c1838bf286b14eb708cda2062957
vikas <vikaskumar638727@gmail.com>
2024-11-19 04:47:13 +0530

    add type

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,25 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import GlobalNav from './components/Global/GlobalNav';
-// import GlobalPage from './pages/GlobalPage';
-// import MnPage from './pages/MnPage';
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <div className="bg-slate-950 text-white">
-//         {/* Global Navbar will be present on all pages */}
-//         <GlobalNav />
-        
-//         <div className="p-4">
-//           <Routes>
-//             <Route path="/" element={<GlobalPage />} /> {/* Default route */}
-//             <Route path="/mn/*" element={<MnPage />} /> {/* MnPage for /mn routes */}
-//           </Routes>
-//         </div>
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GlobalNav from './components/Global/GlobalNav';
+import GlobalPage from './pages/GlobalPage';
+import MnPage from './pages/MnPage';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <div className="bg-slate-950 text-white">
+        {/* Global Navbar will be present on all pages */}
+        <GlobalNav />
+        
+        <div className="p-4">
+          <Routes>
+            <Route path="/" element={<GlobalPage />} /> {/* Default route */}
+            <Route path="/mn/*" element={<MnPage />} /> {/* MnPage for /mn routes */}
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
