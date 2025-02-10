5b3005e2ac8018364ba4061cfa3c213c2d2b45b4
vikas <vikaskumar638727@gmail.com>
2025-02-10 23:44:43 +0530

    course id

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,30 +6,31 @@ import { redirect } from "next/navigation";
 
 const CourseIdPage = async({params}: {params: {courseId: string}}) => {
     const {userId}= await auth();
-    // if(!userId){
-    //     return redirect("/");
-    // }
-    // const course=await db.course.findUnique({where:{id: params.courseId}});
-    // if(!course){
-    //     return redirect("/");
-    // }
-    // const requiredFields=[
-    //     course.title,
-    //     course.imageUrl,
-    //     course.price,
-    //     course.categoryId,
-    //     course.description
+    if(!userId){
+        return redirect("/");
+    }
+    const course=await db.course.findUnique({where:{id: params.courseId}});
+    if(!course){
+        return redirect("/");
+    }
+    const requiredFields=[
+        course.title,
+        course.imageUrl,
+        course.price,
+        course.categoryId,
+        course.description
 
-    // ];
-    // const totalFiels=requiredFields.length;
-    // const compeletedFields=requiredFields.filter(Boolean).length;
-    // const completionText=`(${compeletedFields}/$)`
+    ];
+    const totalFields=requiredFields.length;
+    const compeletedFields=requiredFields.filter(Boolean).length;
+    const completionText=`(${compeletedFields}/${totalFields})`
     return ( 
         <div className="p-6">
            <div className="flex items-center justify-between">
             <div className="flex flex-col gap-y-2">
-                <h1>asjdhfjhas</h1>
+                <h1>Course Setup</h1>
             </div>
+            <span className="text-sm text-slate-700">Complete all fields{completionText}</span>
            </div>
         </div>
      );
